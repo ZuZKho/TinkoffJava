25e864307f5b5d3136cba97e38b5931d84d5eb55
Matvey Goncharov <m.goncharov1@g.nsu.ru>
2023-11-16 22:01:02 +0700

    first version

---
+++ src/main/java/edu/project3/tableRenderers/AdocTableRenderer.java
@@ -0,0 +1,54 @@
+package edu.project3.tableRenderers;
+
+import java.util.List;
+
+public class AdocTableRenderer implements TableRenderer {
+
+    private static String getTableHeaderLine(Object[] row) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < row.length; i++) {
+            String value = row[i].toString();
+            stringBuilder.append("| ");
+            stringBuilder.append(value);
+            stringBuilder.append(" ");
+        }
+        stringBuilder.append("\n");
+        stringBuilder.append("\n");
+
+        return stringBuilder.toString();
+    }
+
+    private static String getTableLine(Object[] row) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < row.length; i++) {
+            String value = row[i].toString();
+            stringBuilder.append("| ");
+            stringBuilder.append(value);
+            stringBuilder.append("\n");
+        }
+        stringBuilder.append("\n");
+
+        return stringBuilder.toString();
+    }
+
+    public String render(String header, int tableWidth, List<Object[]> table) {
+        StringBuilder stringBuilder = new StringBuilder("=== " + header + "\n\n");
+        stringBuilder.append("[cols=\"1");
+        for (int i = 1; i < tableWidth; i++) {
+            stringBuilder.append(", 1");
+        }
+        stringBuilder.append("\"]\n");
+        stringBuilder.append("|===\n");
+
+        stringBuilder.append(getTableHeaderLine(table.get(0)));
+        for (int row = 1; row < table.size(); row++) {
+            stringBuilder.append(getTableLine(table.get(row)));
+        }
+
+        stringBuilder.append("|===\n\n");
+        return stringBuilder.toString();
+    }
+
+}
