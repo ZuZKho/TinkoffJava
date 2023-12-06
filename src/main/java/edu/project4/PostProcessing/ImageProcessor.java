b1e68a830b7a8b9178253f748452dc07a21d9a2c
Matvey Goncharov <m.goncharov1@g.nsu.ru>
2023-12-06 09:26:55 +0700

    project 4

---
+++ src/main/java/edu/project4/PostProcessing/ImageProcessor.java
@@ -0,0 +1,10 @@
+package edu.project4.PostProcessing;
+
+import edu.project4.shared.FractalImage;
+
+@FunctionalInterface
+public
+interface ImageProcessor {
+
+    void process(FractalImage image);
+}
